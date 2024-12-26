61f918eb75cb6607616e1722684ddfa55d018aeb
NQD190523 <duc19052003@gmail.com>
2024-12-27 00:53:05 +0700

    fix(application properties): add redis password variable to production profile

--- src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
+++ src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlShortenApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(UrlShortenApplication.class, args);
 	}
 
