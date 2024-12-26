1f4aaa85e8a2c49592932962d5d7c2c16ebdb51b
NQD190523 <duc19052003@gmail.com>
2024-12-27 00:34:21 +0700

    fix(project): fix the two environments variables in application.properties

--- src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
+++ src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
@@ -3,21 +3,10 @@ package com.challenge.urlShorten;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class UrlShortenApplication {
 
 	public static void main(String[] args) {
-		String environment = System.getenv("ENVIRONMENT"); 
-        
-        if ("development".equalsIgnoreCase(environment)) {
-            Dotenv dotenv = Dotenv.configure()
-                                  .directory("./")
-                                  .load();
-            System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
-            System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
-        }
 		SpringApplication.run(UrlShortenApplication.class, args);
 	}
 
