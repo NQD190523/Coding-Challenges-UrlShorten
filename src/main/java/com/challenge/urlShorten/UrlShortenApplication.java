44496a9e96c6d876e3213a6784e3c4eb8c247da5
NQD190523 <duc19052003@gmail.com>
2024-12-26 23:53:28 +0700

    fix(UrlShortenApplication): devide urlShortenApplication into two environments

--- src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
+++ src/main/java/com/challenge/urlShorten/UrlShortenApplication.java
@@ -9,11 +9,15 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class UrlShortenApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-											.directory("./")
-											.load();
-		System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
-		System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
+		String environment = System.getenv("ENVIRONMENT"); 
+        
+        if ("development".equalsIgnoreCase(environment)) {
+            Dotenv dotenv = Dotenv.configure()
+                                  .directory("./")
+                                  .load();
+            System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
+            System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
+        }
 		SpringApplication.run(UrlShortenApplication.class, args);
 	}
 
